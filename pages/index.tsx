7cb43afa17db3a4770c2c2aa7dbf29081f782191
Ji Kuai <rk427454171@gmail.com>
2023-08-29 23:17:07 +0800

    chore: setup next.js

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+export default function() {
+    return (<>
+        <h1>程式解題社—講義</h1>
+        <h2>
+            <Link href="/handout">Go to example</Link>
+        </h2>
+    </>);
+};
