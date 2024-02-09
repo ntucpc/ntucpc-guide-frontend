2bc4ba56fa2173abafa38fc871dcf4037691901c
PixelCat <b12902043@csie.ntu.edu.tw>
2024-02-09 22:48:41 +0800

    feat: added global components
    - added global stylesheet
    - added navigation bar and page footer (still very simple for now)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
+import { Box, Divider } from "@mui/material";
+import { NavBar, NAVBAR_HEIGHT, PageFooter } from "../components";
+import "../styles/global.css";
+import { Container } from "@mui/system";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return (<>
-        <Component {...pageProps} />
-    </>);
+  return (
+    <>
+      <NavBar />
+      <Box sx={{ marginTop: NAVBAR_HEIGHT }}>
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </Box>
+      <Divider />
+      <PageFooter />
+    </>
+  );
 }
---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+ 
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
