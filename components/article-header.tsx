ed67618c902cc9fd2213679627985ec90f42c656
PixelCat <b12902043@csie.ntu.edu.tw>
2024-02-26 00:16:24 +0800

    feat: display level in sections

--- components/article-header.tsx
+++ components/article-header.tsx
@@ -1,8 +1,9 @@
 import { Box, Typography, Breadcrumbs, Link, Stack, Divider } from "@mui/material";
 import { SectionType } from "lib/contents-handler";
-import EditNoteTwoToneIcon from '@mui/icons-material/EditNoteTwoTone';
-import VolunteerActivismTwoToneIcon from '@mui/icons-material/VolunteerActivismTwoTone';
-import EmojiObjectsTwoToneIcon from '@mui/icons-material/EmojiObjectsTwoTone';
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
+import EmojiObjectsIcon from '@mui/icons-material/EmojiObjects';
+import StarHalfIcon from '@mui/icons-material/StarHalf';
 
 export default function ArticleHeader({section}: {section: SectionType}) {
     return (
@@ -19,20 +20,26 @@ export default function ArticleHeader({section}: {section: SectionType}) {
                 <Typography variant="h3">{section.d_section.title}</Typography>
                 {section.d_section.authors.length > 0 ? 
                     <Stack direction="row" spacing={1} divider={<Divider orientation="vertical" flexItem />} alignItems="center">
-                        <EditNoteTwoToneIcon/>
+                        <EditNoteIcon/>
                         <Typography variant="subtitle1">作者：{section.d_section.authors.join("、")}</Typography>
                     </Stack>
                 : <></>}
                 {section.d_section.contributors.length > 0 ? 
                     <Stack direction="row" spacing={1} divider={<Divider orientation="vertical" flexItem />} alignItems="center">
-                        <VolunteerActivismTwoToneIcon/>
+                        <VolunteerActivismIcon/>
                         <Typography variant="subtitle1">共同作者：{section.d_section.contributors.join("、")}</Typography>
                     </Stack>
                 : <></>}
                 {section.d_section.prerequisites.length > 0 ? 
                     <Stack direction="row" spacing={1} divider={<Divider orientation="vertical" flexItem />} alignItems="center">
-                        <EmojiObjectsTwoToneIcon/>
+                        <EmojiObjectsIcon/>
                         <Typography variant="subtitle1">先備知識：{section.d_section.prerequisites.join("、")}</Typography>
+                    </Stack>
+                : <></>}
+                {section.d_level ? 
+                    <Stack direction="row" spacing={1} divider={<Divider orientation="vertical" flexItem />} alignItems="center">
+                        <StarHalfIcon/>
+                        <Typography variant="subtitle1">難度分級：{section.d_level.title}</Typography>
                     </Stack>
                 : <></>}
             </Stack>
