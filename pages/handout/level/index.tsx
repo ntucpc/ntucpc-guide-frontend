5da86410f938743c19991a38a5c902e73e1c3277
PixelCat <b12902043@csie.ntu.edu.tw>
2024-03-16 01:14:52 +0800

    refactor: handout/level

--- pages/handout/level/index.tsx
+++ pages/handout/level/index.tsx
@@ -4,28 +4,47 @@ import { getLevels, LevelType } from 'lib/contents-handler';
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { Typography } from '@mui/material';
 
-export const getStaticProps: GetStaticProps<{levels: LevelType[]}> = async () => {
-    const levels = getLevels();
+type SectionProps = {
+    id: string;
+    url: string;
+    title: string;
+};
+type LevelProps = {
+    id: string;
+    url: string;
+    title: string;
+    sections: SectionProps[];
+};
+
+export const getStaticProps: GetStaticProps<{levels: LevelProps[]}> = async () => {
+    const levels = getLevels().map((lvl) => {
+        const sections = lvl.sections.map((sec) => {
+            const {id, url, title} = sec;
+            return {id, url, title};
+        })
+        const {id, url, title} = lvl;
+        return { id, url, title, sections, };
+    });
     return { props: { levels }};
 }
 export default function Pages({ levels }: InferGetStaticPropsType<typeof getStaticProps>) {
     const list: React.JSX.Element[] = [];
     for (const level of levels) {
-        list.push(<Typography variant='h4' key={`${level.d_level.id}-title`}>
-            {level.d_level.title}
+        list.push(<Typography variant='h4' key={`${level.id}-title`}>
+            {level.title}
             {/* <Link href={`handout/${chapter_name}`}>{chapter_name}</Link> */}
         </Typography>);
 
         const section_elems: React.JSX.Element[] = [];
-        for (const section of level.d_sections) {
+        for (const section of level.sections) {
             section_elems.push(
-                <li key={`${level.d_level.id}-${section.id}`}>
+                <li key={`${level.id}-${section.id}`}>
                     <Link href={section.url}>{section.title}</Link>
                 </li>
             );
         }
 
-        list.push(<ul key={`${level.d_level.id}-sections`}>
+        list.push(<ul key={`${level.id}-sections`}>
             { section_elems }
         </ul>);
     }
