b24669107c2d2c55e0b007f159485f91888cb66a
PixelCat <b12902043@csie.ntu.edu.tw>
2024-03-16 01:23:13 +0800

    refactor: handout/[chapter]

--- pages/handout/[chapter]/index.tsx
+++ pages/handout/[chapter]/index.tsx
@@ -3,31 +3,48 @@ import Link from 'next/link';
 import { ChapterType, getChapterByName, getChapters, getSectionsByChapter, SectionType } from 'lib/contents-handler';
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next';
 
+type SectionProps = {
+    id: string;
+    url: string;
+    title: string;
+};
+type ChapterProps = {
+    title: string;
+    sections: SectionProps[];
+};
+
 export const getStaticPaths: GetStaticPaths = async () => {
-    const paths = getChapters().map(chapter => ({params: {chapter: chapter.d_chapter.id}}));
+    const paths = getChapters().map(chapter => ({params: {chapter: chapter.id}}));
     return {
         paths,
         fallback: false,
     };
 }
-export const getStaticProps: GetStaticProps<{chapter: ChapterType}> = async ({ params }) => {
+export const getStaticProps: GetStaticProps<{chapter: ChapterProps}> = async ({ params }) => {
     if (!params)
         throw Error('param not exist in [chapter]');
-    const chapter = getChapterByName(params.chapter as string);
+    const chapter_obj = getChapterByName(params.chapter as string);
+    const chapter = {
+        title: chapter_obj.title,
+        sections: chapter_obj.sections.map((sec) => {
+            const {id, url, title} = sec;
+            return {id, url, title};
+        }),
+    };
     return { props: { chapter } };
 }
 export default function Pages({ chapter }: InferGetStaticPropsType<typeof getStaticProps>) {
     const sections: React.JSX.Element[] = [];
-    for (const d_section of chapter.d_sections) {
+    for (const section of chapter.sections) {
         sections.push(
-            <li key={`${chapter}-${d_section}`}>
-                <Link href={`${d_section.url}`}>{d_section.title}</Link>
+            <li key={`${chapter}-${section.id}`}>
+                <Link href={`${section.url}`}>{section.title}</Link>
             </li>
         );
     }
 
     return (<>
-        <h1>{chapter.d_chapter.title}</h1>
+        <h1>{chapter.title}</h1>
         <ul>
             {sections}
         </ul>
