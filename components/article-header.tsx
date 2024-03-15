21236970c769cb762a36cf2e1a861b8d828e9a3d
PixelCat <b12902043@csie.ntu.edu.tw>
2024-03-16 01:43:18 +0800

    refactor: article header and footer

--- components/article-header.tsx
+++ components/article-header.tsx
@@ -5,7 +5,23 @@ import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
 import EmojiObjectsIcon from '@mui/icons-material/EmojiObjects';
 import StarHalfIcon from '@mui/icons-material/StarHalf';
 
-export default function ArticleHeader({section}: {section: SectionType}) {
+export type ArticleHeaderPropsType = {
+    chapter: {
+        id: string;
+        url: string;
+    };
+    section: {
+        title: string;
+        authors: string[];
+        contributors: string[];
+        prerequisites: string[];
+    };
+    level: {
+        title: string;
+    };
+};
+
+export default function ArticleHeader({chapter, section, level}: ArticleHeaderPropsType) {
     return (
         <>
             <Stack spacing={2} margin={2}>
@@ -13,33 +29,33 @@ export default function ArticleHeader({section}: {section: SectionType}) {
                     <Breadcrumbs aria-label="breadcrumb">
                         <Link underline="hover" color="inherit" href="/">NTUCPC Guide</Link>
                         {/* <Link underline="hover" color="inherit" href={section.handout_url}>Handout</Link> */}
-                        <Link underline="hover" color="inherit" href={section.d_chapter?.url}>{section.d_chapter?.id}</Link>
-                        <Typography color="text.primary">{section.d_section.title}</Typography>
+                        <Link underline="hover" color="inherit" href={chapter.url}>{chapter.id}</Link>
+                        <Typography color="text.primary">{section.title}</Typography>
                     </Breadcrumbs>
                 </Box>
-                <Typography variant="h3">{section.d_section.title}</Typography>
-                {section.d_section.authors.length > 0 ? 
+                <Typography variant="h3">{section.title}</Typography>
+                {section.authors.length > 0 ? 
                     <Stack direction="row" spacing={1} divider={<Divider orientation="vertical" flexItem />} alignItems="center">
                         <EditNoteIcon/>
-                        <Typography variant="subtitle1">作者：{section.d_section.authors.join("、")}</Typography>
+                        <Typography variant="subtitle1">作者：{section.authors.join("、")}</Typography>
                     </Stack>
                 : <></>}
-                {section.d_section.contributors.length > 0 ? 
+                {section.contributors.length > 0 ? 
                     <Stack direction="row" spacing={1} divider={<Divider orientation="vertical" flexItem />} alignItems="center">
                         <VolunteerActivismIcon/>
-                        <Typography variant="subtitle1">共同作者：{section.d_section.contributors.join("、")}</Typography>
+                        <Typography variant="subtitle1">共同作者：{section.contributors.join("、")}</Typography>
                     </Stack>
                 : <></>}
-                {section.d_section.prerequisites.length > 0 ? 
+                {section.prerequisites.length > 0 ? 
                     <Stack direction="row" spacing={1} divider={<Divider orientation="vertical" flexItem />} alignItems="center">
                         <EmojiObjectsIcon/>
-                        <Typography variant="subtitle1">先備知識：{section.d_section.prerequisites.join("、")}</Typography>
+                        <Typography variant="subtitle1">先備知識：{section.prerequisites.join("、")}</Typography>
                     </Stack>
                 : <></>}
-                {section.d_level ? 
+                {level ? 
                     <Stack direction="row" spacing={1} divider={<Divider orientation="vertical" flexItem />} alignItems="center">
                         <StarHalfIcon/>
-                        <Typography variant="subtitle1">難度分級：{section.d_level.title}</Typography>
+                        <Typography variant="subtitle1">難度分級：{level.title}</Typography>
                     </Stack>
                 : <></>}
             </Stack>
