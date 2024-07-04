f909f100c0f65794075418a68800fd481f5b17e5
wiwiho <wiwiho1211@gmail.com>
2024-07-05 05:30:41 +0800

    fix: favicon
    Why is it so hard...

--- components/layout.tsx
+++ components/layout.tsx
@@ -39,7 +39,16 @@ export function Layout(props: Props) {
         <Head>
             <title>{title}</title>
             <meta property="og:title" content={title} />
-            <link rel="icon" type="image/png" href="/favicon.png" />
+            {/* https://realfavicongenerator.net/ */}
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+            <link rel="manifest" href="/favicon/site.webmanifest" />
+            <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#6d5fff" />
+            <link rel="shortcut icon" href="/favicon/favicon.ico" />
+            <meta name="msapplication-TileColor" content="#000000" />
+            <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+            <meta name="theme-color" content="#ffffff"></meta>
         </Head>
         <NavBar />
         {props.children}
