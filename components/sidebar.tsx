f9b6bb55b4cfc4e878b378e35e31a7a55a2521ce
wiwiho <wiwiho1211@gmail.com>
2024-07-08 17:35:35 +0800

    feat: better sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,7 +3,7 @@ import { Chapter } from "@/lib/chapters"
 import { Topic, VirtualTopic } from "@/lib/topics"
 import { WrappedLink } from "@/ntucpc-website-common-lib/components/common"
 import { ArticleProps } from "@/pages/[topic]/[article]"
-import { faAnglesRight, faX, faXmark } from "@fortawesome/free-solid-svg-icons"
+import { faAnglesRight, faArrowLeft, faChevronLeft, faChevronRight, faX, faXmark } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Dispatch, MouseEventHandler, ReactNode, SetStateAction, useState } from "react"
 
@@ -15,12 +15,29 @@ type SidebarTabProps = {
 
 type SidebarEntryProps = {
     children: ReactNode,
-    href: string,
+    effect: string | (() => (void)),
     active: boolean
 }
 
 type SidebarSectionProps = {
     text: string
+}
+
+type SidebarTitleProps = {
+    text: string
+    left?: (() => (void)) | undefined
+    right?: (() => (void)) | undefined
+    page?: boolean
+}
+
+type SidebarTableButtonProps = {
+    effect: () => (void)
+    children: ReactNode
+}
+
+type SidebarPageButtonProps = {
+    effect: (() => (void)) | undefined
+    children: ReactNode
 }
 
 enum Tab {
@@ -37,6 +54,28 @@ function SidebarTab(props: SidebarTabProps) {
     </div>
 }
 
+function SidebarTableButton(props: SidebarTableButtonProps) {
+    return <div className="mb-1">
+        <span className="px-2 py-1 text-indigo-600 font-semibold hover:bg-indigo-100 cursor-pointer" onClick={props.effect}>
+            {props.children}
+        </span>
+    </div>
+}
+
+function SidebarPageButton(props: SidebarPageButtonProps) {
+    return <span className={`px-2 rounded ${props.effect ? "text-indigo-500 hover:text-indigo-700 hover:bg-indigo-100 cursor-pointer"
+        : "text-gray-500"}`}
+        onClick={props.effect}>{props.children}</span>
+}
+
+function SidebarTitle(props: SidebarTitleProps) {
+    return <div className={`text-lg font-semibold mb-3 flex justify-between ${!props.page ? "pl-2" : ""}`}>
+        {props.page ? <SidebarPageButton effect={props.left}><FontAwesomeIcon icon={faChevronLeft} /></SidebarPageButton> : <></>}
+        {props.text}
+        {props.page ? <SidebarPageButton effect={props.right}><FontAwesomeIcon icon={faChevronRight} /></SidebarPageButton> : <></>}
+    </div>
+}
+
 function SidebarSection(props: SidebarSectionProps) {
     return <div className="font-semibold mb-2 mt-3">{props.text}</div>;
 }
@@ -45,69 +84,137 @@ function SidebarEntry(props: SidebarEntryProps) {
     return <div className={`w-full border-l-4 hover:border-indigo-600 hover:bg-indigo-100 hover:text-indigo-600 hover:font-semibold
             ${props.active ? "border-indigo-600 bg-indigo-100 text-indigo-600 font-semibold" : 
                 "border-gray-200"}`}>
-        <WrappedLink className="pl-2 py-2 block" href={props.href}>{props.children}</WrappedLink>
+        {typeof props.effect === "string" ? 
+            <WrappedLink className="pl-2 py-2 block" href={props.effect}>{props.children}</WrappedLink> :
+            <div className="pl-2 py-2 cursor-pointer" onClick={props.effect}>{props.children}</div>
+        }
     </div>
 }
 
 export function Sidebar(props: ArticleProps) {
-    const [displayTab, setDisplayTab] = useState(Tab.Article);
-    const [displaySidebar, setDisplaySidebar] = useState(false);
+    const allTopics: VirtualTopic[] = []
+    props.topicStructure.forEach((virtualTopicGroup) => {
+        virtualTopicGroup.topics.forEach((virtualTopic) => 
+            allTopics.push(virtualTopic) 
+        )
+    })
+    const [displayTab, setDisplayTab] = useState(Tab.Article)
+    const [displaySidebar, setDisplaySidebar] = useState(false)
+    const articleChapter = props.chapterStructure.find(
+            (chapter) => chapter.code === props.virtualArticle.chapterCode)
+    const articleTopic = allTopics.find(
+            (topic) => topic.code === props.virtualArticle.topicCode)
+    const [activeChapter, setActiveChapter] =
+        useState(articleChapter)
+    const [activeTopic, setActiveTopic] =
+        useState(articleTopic)
 
     const chapterToC = (() => {
         const toC: ReactNode[] = []
         let num = 0
-        props.chapterStructure.find(
-            (chapter) => chapter.code === props.virtualArticle.chapterCode
-        )?.topics.forEach((virtualTopic) =>{
-            toC.push(<SidebarSection key={num++} text={virtualTopic.displayTitle} />)
-            virtualTopic.articles.forEach((virtualArticle) => {
-                toC.push(<SidebarEntry key={num++} href={`/${virtualArticle.code}`}
-                            active={virtualArticle.code === props.virtualArticle.code}
-                             >{virtualArticle.articleDisplayTitle}</SidebarEntry>)
+        if (activeChapter) {
+            const index = props.chapterStructure.findIndex((chapter) => chapter.code === activeChapter.code)
+            const previousChapter = index > 0 ? props.chapterStructure[index - 1] : undefined
+            const nextChapter = index + 1 < props.chapterStructure.length ? props.chapterStructure[index + 1] : undefined
+
+            toC.push(<SidebarTableButton key={num++} effect={() => { setActiveChapter(undefined) }}>章節目錄</SidebarTableButton>)
+
+            toC.push(<SidebarTitle key={num++} text={activeChapter.displayTitle}
+                page={true} 
+                left={previousChapter ? () => { setActiveChapter(previousChapter) } : undefined}
+                right={nextChapter ? () => {setActiveChapter(nextChapter)} : undefined} />)
+            
+            activeChapter.topics.forEach((virtualTopic) =>{
+                toC.push(<SidebarSection key={num++} text={virtualTopic.displayTitle} />)
+                virtualTopic.articles.forEach((virtualArticle) => {
+                    toC.push(<SidebarEntry key={num++} effect={`/${virtualArticle.code}`}
+                                active={virtualArticle.code === props.virtualArticle.code}
+                                >{virtualArticle.articleDisplayTitle}</SidebarEntry>)
+                })
             })
-        })
+        }
+        else {
+            toC.push(<SidebarTableButton key={num++} effect={() => {setActiveChapter(articleChapter)}}>
+                <FontAwesomeIcon icon={faChevronLeft}/> {articleChapter?.displayTitle}
+            </SidebarTableButton>)
+            toC.push(<SidebarTitle key={num++} text="章節目錄" />)
+
+            props.chapterStructure.forEach((chapter) => {
+                toC.push(<SidebarEntry key={num++} effect={() => {setActiveChapter(chapter)}}
+                        active={chapter.code === props.virtualArticle.chapterCode}>
+                    {chapter.displayTitle}
+                </SidebarEntry>)
+            })
+        }
         return toC;
     })();
     const topicToC = (() => {
         const toC: ReactNode[] = [];
         let num = 0
-        const compareTopic = (virtualTopic: VirtualTopic) => virtualTopic.code === props.virtualArticle.topicCode
-        props.topicStructure.find(
-            (topicGroup) => 
-                topicGroup.topics.find(compareTopic)
-        )?.topics.find(compareTopic)?.articles.forEach((virtualArticle) => {
-            toC.push(<SidebarEntry key={num++} href={`/${virtualArticle.code}`}
+        if (activeTopic) {
+            toC.push(<SidebarTableButton key={num++} effect={() => { setActiveTopic(undefined) }}>主題目錄</SidebarTableButton>)
+            toC.push(<SidebarTitle key={num++} text={activeTopic.displayTitle} />)
+            activeTopic.articles.forEach((virtualArticle) => {
+                toC.push(<SidebarEntry key={num++} effect={`/${virtualArticle.code}`}
                     active={virtualArticle.code === props.virtualArticle.code}>
-                        {virtualArticle.articleDisplayTitle}
+                    {virtualArticle.articleDisplayTitle}
+                </SidebarEntry>)
+            })
+        }
+        else {
+            toC.push(<SidebarTableButton key={num++} effect={() => {setActiveTopic(articleTopic)}}>
+                <FontAwesomeIcon icon={faChevronLeft}/> {articleTopic?.displayTitle}
+            </SidebarTableButton>)
+            toC.push(<SidebarTitle key={num++} text="主題目錄" />)
+            let lastGroup = false
+            props.topicStructure.forEach((topicGroup) => {
+                if (topicGroup.single) {
+                    if (lastGroup) {
+                        toC.push(<SidebarSection key={num++} text="" />)
+                        lastGroup = false
+                    }
+                    const topic = topicGroup.topics[0]!
+                    toC.push(<SidebarEntry key={num++} effect={() => {setActiveTopic(topic)}}
+                    active={topic.code === props.virtualArticle.topicCode}>
+                        {topic.displayTitle}
                     </SidebarEntry>)
-        })
+                }
+                else {
+                    lastGroup = true
+                    toC.push(<SidebarSection key={num++} text={topicGroup.title}/>)
+                    topicGroup.topics.forEach((topic) => {
+                        toC.push(<SidebarEntry key={num++} effect={() => { setActiveTopic(topic) }}
+                            active={topic.code === props.virtualArticle.topicCode}>
+                            {topic.displayTitle}
+                        </SidebarEntry>)
+                    })
+                }
+            })
+        }
         return toC;
     })();
     const articleToC = (() => {
         const toC = [];
         let num = 0;
+        toC.push(<SidebarTitle key={num++} text={props.virtualArticle.articleDisplayTitle} />)
         for (const section of props.sections) {
             if (section.depth >= 3) continue;
 
-            toC.push(<SidebarEntry key={num++} href={`#section-${section.code}`} active={false}>
+            toC.push(<SidebarEntry key={num++} effect={`#section-${section.code}`} active={false}>
                 <div className={section.depth == 2 ? "ml-3" : ""}>{section.text}</div>
             </SidebarEntry>);
         }
         return toC;
     })();
-    let title: string;
     let toC: ReactNode[] = [];
     switch(displayTab) {
         case Tab.Article:
-            title = props.virtualArticle.articleDisplayTitle;
             toC = articleToC;
             break;
         case Tab.Chapter:
-            title = props.virtualArticle.chapterDisplayTitle;
             toC = chapterToC;
             break;
         case Tab.Topic:
-            title = props.virtualArticle.topicDisplayTitle;
             toC = topicToC;
             break;
     }
@@ -119,24 +226,20 @@ export function Sidebar(props: ArticleProps) {
         </div>
         <div className={`fixed text-black z-50 w-72 h-screen pb-40 overflow-y-scroll bg-slate-50 ${displaySidebar ? "" : "max-lg:hidden"}`}>
             <div className="m-5">
-                <div className="flex justify-evenly mt-8">
+                <div className="flex justify-evenly mt-8 mb-5">
                     <SidebarTab text="本文" onClick={() => setDisplayTab(Tab.Article)} active={displayTab === Tab.Article} />
                     <SidebarTab text="章節" onClick={() => setDisplayTab(Tab.Chapter)} active={displayTab === Tab.Chapter} />
                     <SidebarTab text="主題" onClick={() => setDisplayTab(Tab.Topic)} active={displayTab === Tab.Topic} />
                 </div>
                 <div>
-                    <div className="text-lg font-semibold mt-7 mb-3">
-                        {title}
-                    </div>
-
                     {toC}
                 </div>
             </div>
-            <div className={`fixed left-72 top-20 z-50 w-10 h-10 flex items-center 
-                    justify-center cursor-pointer lg:hidden ${displaySidebar ? "" : "hidden"}`}
-                onClick={() => { setDisplaySidebar(false) }}>
-                <FontAwesomeIcon className="text-lg" icon={faXmark} />
-            </div>
+        </div>
+        <div className={`fixed left-72 top-20 z-50 w-10 h-10 flex items-center 
+                justify-center cursor-pointer lg:hidden ${displaySidebar ? "" : "hidden"}`}
+            onClick={() => { setDisplaySidebar(false) }}>
+            <FontAwesomeIcon className="text-lg" icon={faXmark} />
         </div>
     </aside>
 }
