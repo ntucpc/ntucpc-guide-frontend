6b7c27cbe84a2e9c30722de2dca51d0c39fe818d
wiwiho <wiwiho1211@gmail.com>
2026-02-18 01:45:37 +0800

    fix: content overflow when screen is small

--- app/(articles)/layout.tsx
+++ app/(articles)/layout.tsx
@@ -7,7 +7,7 @@ import { ReactNode } from "react";
 
 export default function ArticleLayout({children}: {children: ReactNode}) {
     return <>
-        <div className="flex justify-center items-start gap-10 max-w-[1200px] mx-auto px-4">
+        <div className="xl:flex justify-center items-start gap-10 max-w-[1200px] mx-auto">
             <Sidebar/>
             <ContentBody>
                 {children}
