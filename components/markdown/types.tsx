8fc25368b2793d6772f527472d336a8c55217f22
wiwiho <wiwiho1211@gmail.com>
2026-05-14 19:41:55 +0800

    chore: remove files unused after refactoring

--- components/markdown/types.tsx
+++
@@ -1,6 +0,0 @@
-import { MDXRemoteSerializeResult } from "next-mdx-remote";
-
-export type MarkdownContextType = {
-    mdx_path: string;
-    contents_mapping: Map<string, MDXRemoteSerializeResult>;
-};
